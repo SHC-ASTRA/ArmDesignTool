3afeb6c66668b76f05e0c1fd157289b836ec002f
Thomas <trb0023@uah.edu>
2020-10-23 19:53:11 -0500

    Changed baseline design to make it use smaller actuators

--- LinearActuatorSim.m
+++ LinearActuatorSim.m
@@ -3,10 +3,10 @@
 axis1.length = 0.8;                 %Length of segment (m)
 axis1.originMass = 0;               %Mass of segment concentrated at base (kg)
 axis1.lengthMass = 3+4.2*0.453592;  %Mass of segment concentrated at midpoint (kg)
-axis1.AB_a = -0.75;                 %Actuator base anchor parallel offset (m)
-axis1.AB_r = -0.2;                  %Actuator base anchor perpendicular offset (m)
+axis1.AB_a = -0.55;                 %Actuator base anchor parallel offset (m)
+axis1.AB_r = -0.17;                  %Actuator base anchor perpendicular offset (m)
 axis1.AT_a = -0.075;                %Actuator tip anchor parallel offset (m)
-axis1.AT_r = -0.2;                  %Actuator tip anchor perpendicular offset (m)
+axis1.AT_r = -0.17;                  %Actuator tip anchor perpendicular offset (m)
 axis1.angle = 0;                    % This and all remaining values are IK outputs
 axis1.B_x = 0;
 axis1.B_y = 0;
@@ -20,10 +20,10 @@ axis1.AT_y = 0;
 axis2.length = 0.6;
 axis2.originMass = 0;
 axis2.lengthMass = 3+4.2*0.453592;
-axis2.AB_a = 0;
-axis2.AB_r = 0.175;
-axis2.AT_a = 0.2;
-axis2.AT_r = -0.025;
+axis2.AB_a = 0.2;
+axis2.AB_r = 0.10;
+axis2.AT_a = 0.15;
+axis2.AT_r = -0.075;
 axis2.angle = 0;
 axis2.B_x = 0;
 axis2.B_y = 0;
@@ -37,7 +37,7 @@ axis2.AT_y = 0;
 axis3.length = 0.2;
 axis3.originMass = 0;
 axis3.lengthMass = 3;
-axis3.AB_a = -0.1;
+axis3.AB_a = 0.1;
 axis3.AB_r = 0.1;
 axis3.AT_a = 0;
 axis3.AT_r = 0.1;
@@ -74,8 +74,8 @@ arm = [axis1 axis2 axis3 payload];
 
 %% Variables
 % Angle limits (Length must equal number of non-payload axes)
-angleMins = [-20,-170,-85];
-angleMaxs = [110,-30,75];
+angleMins = [0,-140,-85];
+angleMaxs = [90,-40,70];
 
 % Actuator Specs
 %(currently from Progressive Automation PA-09)
@@ -243,7 +243,12 @@ end
 
 figure(1);
 hold on
-[arm1,~] = forwardKinematics(arm, (angleMins+angleMaxs)./2);
+%[arm1,~] = forwardKinematics(arm, [angleMaxs(1) angleMins(2) 0]);
+%[arm1,~] = forwardKinematics(arm, (angleMaxs+angleMins)/2);
+%drawArm(arm1)
+[arm1,~] = forwardKinematics(arm, angleMaxs);
+drawArm(arm1)
+[arm1,~] = forwardKinematics(arm, angleMins);
 drawArm(arm1)
 j = boundary(endPosList,1);
 plot(endPosList(j,1),endPosList(j,2))
